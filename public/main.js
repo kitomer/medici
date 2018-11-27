96ef1d38efa80f44fa0badf03a5193b8c7ad5070
kitomer <tokirc@gmx.net>
2018-11-27 06:01:44 +0100

    first version of fixed detail when scrolling

--- public/main.js
+++ public/main.js
@@ -14,6 +14,52 @@ function stuff( str, id )
 	}
 }
 
+function isElementInViewport( el )
+{
+	// special bonus for those using jQuery
+	if (typeof jQuery === "function" && el instanceof jQuery) {
+		el = el[0];
+	}
+	var rect = el.getBoundingClientRect();
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+	);
+}
+
+var isDetailInView = true;
+function scrollDetail()
+{
+	var isInView = !! isElementInViewport($('#detailtop'));
+	if( ! isDetailInView && isInView ) {
+		console.log("in!");
+		hideFixedDetail();
+	}
+	else if( isDetailInView && ! isInView ) {
+		console.log('out!');
+		showFixedDetail();
+	}
+	isDetailInView = isInView;
+}
+
+function hideFixedDetail()
+{
+	$('#detailfix').style.display = 'none';
+}
+
+function showFixedDetail()
+{
+	$('#detailfix').style.display = 'block';	
+}
+
+function initBody()
+{
+	toggleMenu();
+	scrollDetail();
+}
+
 /*
  *		info: {
  *      "query" => "..."       # OPTIONAL!
